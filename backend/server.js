757044df29eb653fa0db32446cf3aedb6262ecaf
Amritesh Mani Tripathi <amriteshmanitripathi@gmail.com>
2025-11-02 21:33:50 +0530

    Day-1

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const port = 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('CodeVerse backend running ✅'));
+app.get('/', (req, res) => {
+  res.send('CodeVerse backend running ✅');
+});
 
-app.listen(3000, () => console.log('Server running on port 3000'));
-
+app.listen(port, () => console.log(`Server running on port ${port}`));
