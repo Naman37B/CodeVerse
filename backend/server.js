df121528316fbd04fef8d4c296ac8dd2333b1a13
Amritesh Mani Tripathi <amriteshmanitripathi8.com>
2025-11-10 22:19:15 +0530

    Made some changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,3 @@
-// const express = require('express');
-// const cors = require('cors');
-// const fs = require('fs');
-// const path = require('path'); 
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-
-// let users = [];
-
-// // Load users from file
-// if (fs.existsSync('data/users.json')) {
-//   users = JSON.parse(fs.readFileSync('data/users.json', 'utf8'));
-// }
-
-// // Signup route
-// app.post('/signup', (req, res) => {
-//   const { username, password } = req.body;
-//   if (users.find(u => u.username === username)) {
-//     return res.json({ message: 'User already exists' });
-//   }
-//   users.push({ username, password });
-//   fs.writeFileSync('data/users.json', JSON.stringify(users, null, 2));
-//   res.json({ message: 'Signup successful!' });
-// });
-
-// // Login route
-// app.post('/login', (req, res) => {
-//   const { username, password } = req.body;
-//   const user = users.find(u => u.username === username && u.password === password);
-//   if (user) res.json({ message: 'Login successful!' });
-//   else res.json({ message: 'Invalid username or password' });
-// });
-
-// app.listen(3000, () => console.log('Server running on port 3000'));
-
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
@@ -75,7 +37,10 @@ app.post('/login', (req, res) => {
   else res.json({ message: 'Invalid username or password' });
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+const PORT = 3000;
 
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
 
 
